32c2128901ff9ec1e46168bf8d73f73d81664593
christophemcguinness <30633753+christophemcguinness@users.noreply.github.com>
2019-06-03 01:23:13 +0100

    Added Files From Upload

---
+++ HomeworkUITests/HomeworkUITests/SearchResultsPage.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HomeworkUITests
+{
+    public class FirstURL
+    {
+        private static readonly By urlElement = By.XPath("//*[@id='rso']/div[1]/div/div[1]/div/div/div[1]/a/h3");
+
+        public IWebElement GetUrlElement(IWebDriver driver)
+        {
+            return driver.FindElement(urlElement);
+        }
+    }
+
+    public class ForthPageText
+    {
+        private static readonly By TextElement = By.XPath("//*[@id='resultStats']");
+
+        public IWebElement GetTextElement(IWebDriver driver)
+        {
+            return driver.FindElement(TextElement);
+        }
+    }
+
+    public class GoogleHomeButton
+    {
+        private static readonly By imageButtonElement = By.XPath("//*[@id='logo']");
+
+        public IWebElement GetUrlElement(IWebDriver driver)
+        {
+            return driver.FindElement(imageButtonElement);
+        }
+    }
+
+    public class HomeHrefText
+    {
+        private static readonly By HrefText = By.XPath("/html/body[@id='gsr']/div[@id='main']/div[@id='cnt']/div[@id='top_nav']/div/div[@id='hdtb']/div[@id='hdtbSum']/div[@id='hdtb-s']/div[@id='hdtb-msb']/div[1]/div[@id='hdtb-msb-vis']/div[@class='hdtb-mitem hdtb-msel hdtb-imb']");
+
+        public IWebElement GetHrefTextElement(IWebDriver driver)
+        {
+            return driver.FindElement(HrefText);
+        }
+    }
+
+    public class ForthPageLink
+    {
+        private static readonly By ForthLink = By.XPath("//*[@id='nav']/tbody/tr/td[5]/a");
+
+        public IWebElement GetForthPageLinkElement(IWebDriver driver)
+        {
+            return driver.FindElement(ForthLink);
+        }
+    }
+
+    public class ToolsElement
+    {
+        private static readonly By ToolsButtonElement = By.XPath("/html/body[@id='gsr']/div[@id='main']/div[@id='cnt']/div[@id='top_nav']/div/div[@id='hdtb']/div[@id='hdtbSum']/div[@id='hdtb-s']/div[@id='hdtb-msb']/div[2]/a[@id='hdtb-tls']");
+        private static readonly By AnyTimeButtonElement = By.XPath("/html/body[@id='gsr']/div[@id='main']/div[@id='cnt']/div[@id='top_nav']/div/div[@id='hdtb']/div[@id='hdtbMenus']/div[@class='hdtb-mn-cont']/div[@class='hdtb-mn-hd'][1]/div[@class='mn-hd-txt']");
+        private static readonly By PastHourButtonElement = By.XPath("/html/body[@id='gsr']/div[@id='main']/div[@id='cnt']/div[@id='top_nav']/div/div[@id='hdtb']/div[@id='hdtbMenus']/div[@class='hdtb-mn-cont']/ul[@class='hdtbU hdtb-mn-o']/li[@id='qdr_h']/a[@class='q qs']");
+        private static readonly By ElementText = By.XPath("/html/body[@id='gsr']/div[@id='main']/div[@id='cnt']/div[@id='top_nav']/div/div[@id='hdtb']/div[@id='hdtbMenus']/div[@class='hdtb-mn-cont']/div[@class='hdtb-mn-hd hdtb-tsel']/div[@class='mn-hd-txt']");
+
+        public IWebElement GetTextElement(IWebDriver driver)
+        {
+            return driver.FindElement(ElementText);
+        }
+        public IWebElement GetToolsElement(IWebDriver driver)
+        {
+            return driver.FindElement(ToolsButtonElement);
+        }
+        public IWebElement GetAnyTimeElement(IWebDriver driver)
+        {
+            return driver.FindElement(AnyTimeButtonElement);
+        }
+        public IWebElement GetPastHourElement(IWebDriver driver)
+        {
+            return driver.FindElement(PastHourButtonElement);
+        }
+    }
+
+    class SearchResultsPage
+    {
+        private static String url = "https://www.google.com/search?source=hp&ei=-BP0XIrTJsewkwXBtrW4Cg&q=test&oq=test&gs_l=psy-ab.12..0l10.925121.925608..926433...0.0..0.71.267.4......0....1..gws-wiz.....0..0i131.mybNr_xUXtE";
+        private IWebDriver driver;
+
+        public SearchResultsPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+
+        public void Visit()
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+        public String GetFirstUrlText()
+        {
+            return GetFirstUrlElement().GetUrlElement(driver).Text;
+        }
+
+        public void ClickURL()
+        {
+            GetFirstUrlElement().GetUrlElement(driver).Click();
+        }
+
+        public void ClickTools()
+        {
+            GetToolsElementClickable().GetToolsElement(driver).Click();
+        }
+
+        public void ClickAnyTime()
+        {
+            GetToolsElementClickable().GetAnyTimeElement(driver).Click();
+        }
+
+        public void ClickHour()
+        {
+            GetToolsElementClickable().GetPastHourElement(driver).Click();
+        }
+
+        public void ClickHome()
+        {
+            GetHomeButton().GetUrlElement(driver).Click();
+        }
+
+        public void ClickForthPageButton()
+        {
+            GetClickableForthPageButton().GetForthPageLinkElement(driver).Click();
+        }
+
+        public string GetTextfromTime()
+        {
+            return GetToolsElementClickable().GetTextElement(driver).Text;
+        }
+
+        public string GetTextfromHomeHref()
+        {
+            return GetHomeHrefText().GetHrefTextElement(driver).Text;
+        }
+
+        public string GetTextfromPageStats()
+        {
+            return TextForthPage().GetTextElement(driver).Text;
+        }
+
+        private FirstURL GetFirstUrlElement()
+        {
+            return new FirstURL();
+        }
+        private ToolsElement GetToolsElementClickable()
+        {
+            return new ToolsElement();
+        }
+        private HomeHrefText GetHomeHrefText()
+        {
+            return new HomeHrefText();
+        }
+        private GoogleHomeButton GetHomeButton()
+        {
+            return new GoogleHomeButton();
+        }
+        private ForthPageLink GetClickableForthPageButton()
+        {
+            return new ForthPageLink();
+        }
+
+        private ForthPageText TextForthPage()
+        {
+            return new ForthPageText();
+        }
+
+    }
+}
---
+++ HomeworkUITests/HomeworkUITests/TestClass.cs
@@ -0,0 +1,165 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HomeworkUITests
+{
+    [TestFixture]
+    public class TestClass
+
+    {
+
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void InitDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [OneTimeTearDown]
+        public void Quit()
+        {
+            driver.Quit(); // kill instance of driver
+        }
+
+
+
+        [Test]
+        public void HomePageLoadsSucessfully()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            string buttonText = page.GetLuckyButtonText();
+            Console.WriteLine(buttonText);
+            Assert.IsTrue(buttonText.Contains("I'm Feeling Lucky"), buttonText + " doesn't contains 'I'm Feeling Lucky'");
+
+        }
+
+        [Test]
+        public void FromHomePageMoveToAbout()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.clickAboutButton();
+            AboutPage page2 = new AboutPage(driver);
+            String aboutText = page2.getText();
+            Assert.IsTrue(aboutText.Contains("organise"), aboutText + " doesn't contains 'organise'");
+        }
+
+        [Test]
+        public void ConfrimWeCanSearch()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.setSearchText("Test");
+            page.ClickSearchButton();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            String topUrlText = page2.GetFirstUrlText();
+            Assert.IsTrue(topUrlText.Contains("Speedtest by Ookla - The Global Broadband Speed Test"), topUrlText + " doesn't contains 'Speedtest by Ookla - The Global Broadband Speed Test'");
+
+        }
+
+        [Test]
+        public void OpeningFirstPageOnSearch()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.setSearchText("Test");
+            page.ClickSearchButton();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            page2.ClickURL();
+            SpeedTestPage page3 = new SpeedTestPage(driver);
+            String footertext = page3.getFooterText();
+            Assert.IsTrue(footertext.Contains("Ookla®, Speedtest®, and Speedtest Intelligence® are among some of the federally registered trademarks of Ookla, LLC and may only be used with explicit written permission. © 2006-2019 Ookla, LLC. All Rights Reserved"), footertext + " doesn't contains 'Ookla®, Speedtest®, and Speedtest Intelligence® are among some of the federally registered trademarks of Ookla, LLC and may only be used with explicit written permission. © 2006-2019 Ookla, LLC. All Rights Reserved'");
+
+        }
+
+        [Test]
+        public void ChangeLanguageInSettings()
+        {
+            LanguagePage page = new LanguagePage(driver);
+            page.Visit();
+            page.clickRadioButton("Spanish");
+            page.clickSave();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            string hrefText = page2.GetTextfromHomeHref();
+            Assert.IsTrue(hrefText.Contains("Todo"), hrefText + " doesn't contains 'Todo");
+        }
+
+        [Test]
+        public void ChangeLanguageInSettingsAndChangeBack()
+        {
+            LanguagePage page = new LanguagePage(driver);
+            page.Visit();
+            page.clickRadioButton("Spanish");
+            page.clickSave();
+            driver.SwitchTo().Alert().Accept();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            page.Visit();
+            page.clickRadioButton("English");
+            page.clickSave();
+            driver.SwitchTo().Alert().Accept();
+            string hrefText = page2.GetTextfromHomeHref();
+            Assert.IsTrue(hrefText.Contains("All"), hrefText + " doesn't contains 'All");
+
+        }
+        [Test]
+        public void FromSearchPageChangeToAnHour()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.setSearchText("Test");
+            page.ClickSearchButton();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            page2.ClickTools();
+            System.Threading.Thread.Sleep(5000);
+            page2.ClickAnyTime();
+            System.Threading.Thread.Sleep(5000);
+            page2.ClickHour();
+            string timeText = page2.GetTextfromTime();
+            Assert.IsTrue(timeText.Contains("Past hour"), timeText + " doesn't contains 'Past Hour");
+
+            //System.Threading.Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void FromSearchPageMoveBackToMainPage()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.setSearchText("Test");
+            page.ClickSearchButton();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            String topUrlText = page2.GetFirstUrlText();
+            Assert.IsTrue(topUrlText.Contains("Speedtest by Ookla - The Global Broadband Speed Test"), topUrlText + " doesn't contains 'Speedtest by Ookla - The Global Broadband Speed Test'");
+            page2.ClickHome();
+            string buttonText = page.GetLuckyButtonText();
+            Console.WriteLine(buttonText);
+            Assert.IsTrue(buttonText.Contains("I'm Feeling Lucky"), buttonText + " doesn't contains 'I'm Feeling Lucky'");
+        }
+
+        [Test]
+        public void TestMovingToForthPageOfResults()
+        {
+            MainGooglePage page = new MainGooglePage(driver);
+            page.Visit();
+            page.setSearchText("Test");
+            page.ClickSearchButton();
+            SearchResultsPage page2 = new SearchResultsPage(driver);
+            String topUrlText = page2.GetFirstUrlText();
+            Assert.IsTrue(topUrlText.Contains("Speedtest by Ookla - The Global Broadband Speed Test"), topUrlText + " doesn't contains 'Speedtest by Ookla - The Global Broadband Speed Test'");
+            page2.ClickForthPageButton();
+            String statsText = page2.GetTextfromPageStats();
+            Assert.IsTrue(statsText.Contains("4"), statsText + " doesn't contain '4'");
+
+        }
+
+    }
+}
